83d5fa84f232e173cd0be6590e977ef7db1b1de2
ckc <ckc@2u.hk>
2015-11-19 14:02:32 +0800

    init

---
+++ build/generated-sources/ap-source-output/entities/Player_.java
@@ -0,0 +1,17 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-19T13:29:06")
+@StaticMetamodel(Player.class)
+public class Player_ { 
+
+    public static volatile SingularAttribute<Player, String> firstname;
+    public static volatile SingularAttribute<Player, String> jerseynumber;
+    public static volatile SingularAttribute<Player, String> lastspokenwords;
+    public static volatile SingularAttribute<Player, Integer> id;
+    public static volatile SingularAttribute<Player, String> lastname;
+
+}
